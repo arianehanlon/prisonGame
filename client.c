cac5f9e1b49156598fd216b23e0ef682353a5f69
jackieflash <jackie@jackieflash.com>
2017-07-09 22:39:03 -0300

    Small bit of refactoring

--- client.c
+++ client.c
@@ -1,5 +1,10 @@
-//Initial connection setup was referenced from suggested resource codeproject.com
-
+/* Assignment 5: CSCI 3171: Summer 2017
+ *
+ * Program for client to connect to the Prison Game server
+ *
+ * Initial connection setup was referenced from suggested resource
+ * codeproject.com
+ */
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -10,56 +15,44 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
- 
+
 int main(void) {
 
-//begin connection setup
-
-  int sockfd = 0,n = 0;
+  //begin connection setup
+  int sockfd = 0, n = 0;
+  char sendBuff[1024]; //buffer used to contain messages sent to server
   char recvBuff[1024];
   struct sockaddr_in serv_addr;
 
-  //buffer used to contain messages sent to server
-  char sendBuff[1024];
- 
   memset(recvBuff, '0' ,sizeof(recvBuff));
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0)
-    {
-      printf("\n Error : Could not create socket \n");
-      return 1;
-    }
- 
+
+  if((sockfd = socket(AF_INET, SOCK_STREAM, 0))< 0) {
+    printf("\n Error : Could not create socket \n");
+    return 1;
+  }
+
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(5000);
   serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
- 
-  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0)
-    {
-      printf("\n Error : Connect Failed \n");
-      return 1;
-    }
- 
-  while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
-      recvBuff[n] = 0; //terminate message line at the end of the string
-      printf("...%s....\n", recvBuff);
-      if(fputs(recvBuff, stdout) == EOF) 
-    {
-      printf("\n Error : Fputs error");
-    }
+
+  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0) {
+    printf("\n Error : Connect Failed \n");
+    return 1;
+  }
+
+  while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
+    recvBuff[n] = 0; //terminate message line at the end of the string
+    printf("...%s....\n", recvBuff);
+    if(fputs(recvBuff, stdout) == EOF) printf("\n Error : Fputs error");
       //write() to send buffer in order to transmit new message
-      if (strcmp (recvBuff, "I am a guilty cat") == 0)
-      {
-        strcpy(sendBuff, "Prison sentence is nine consecutive life terms");
-        write(sockfd, sendBuff, sizeof(sendBuff)-1);
-      }
-      printf("\n");
+    if (strcmp (recvBuff, "I am a guilty cat") == 0) {
+      strcpy(sendBuff, "Prison sentence is nine consecutive life terms");
+      write(sockfd, sendBuff, sizeof(sendBuff)-1);
     }
- 
-  if( n < 0)
-    {
-      printf("\n Read Error \n");
-    }
- 
+    printf("\n");
+  }
+
+  if( n < 0) printf("\n Read Error \n");
+
   return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,10 @@
-//Initial connection setup was referenced from suggested resource codeproject.com
-
-
+/* Assignment 5: CSCI 3171: Summer 2017
+ *
+ * Program for client to connect to the Prison Game server
+ *
+ * Initial connection setup was referenced from suggested resource
+ * codeproject.com
+ */
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -10,71 +14,50 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
- 
-int main(void)
-{
+
+int main(void) {
   int listenfd = 0,connfd = 0;
-  
   struct sockaddr_in serv_addr;
- 
-  char sendBuff[1025];  
-  int numrv; 
-
-  //buffer used to contain messages received from client
+  char sendBuff[1025];
   char recvBuff[1025];
+  int numrv;
 
- 
   listenfd = socket(AF_INET, SOCK_STREAM, 0);
   printf("socket retrieve success\n");
-  
   memset(&serv_addr, '0', sizeof(serv_addr));
   memset(sendBuff, '0', sizeof(sendBuff));
-      
-  serv_addr.sin_family = AF_INET;    
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
-  serv_addr.sin_port = htons(5000);    
- 
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(5000);
+
   bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
-  
-  if(listen(listenfd, 10) == -1){
+
+  if(listen(listenfd, 10) == -1) {
       printf("Failed to listen\n");
       return -1;
-  }     
-  
-  while(1)
-    {      
-      connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL); // accept awaiting request
+  }
 
-  
-      strcpy(sendBuff, "I am a guilty cat");
-      printf("derp\n");
-      write(connfd, sendBuff, strlen(sendBuff));
-      
-      //
-      int n;
-      while((n = read(listenfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
+  while(1) {
+    connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL); // accept awaiting request
+    strcpy(sendBuff, "I am a guilty cat");
+    printf("derp\n");
+    write(connfd, sendBuff, strlen(sendBuff));
+    int n;
+    while((n = read(listenfd, recvBuff, sizeof(recvBuff)-1)) > 0) {
       recvBuff[n] = 0; //terminate message line at the end of the string
-    
-      if(fputs(recvBuff, stdout) == EOF) 
-    {
-      printf("\n Error : Fputs error");
-    }
+      if(fputs(recvBuff, stdout) == EOF) printf("\n Error : Fputs error");
       //write() to send buffer in order to transmit new message
 
-     /* if (strcmp (recvBuff, "I am a guilty cat") == 0)
-      {
+     /* if (strcmp (recvBuff, "I am a guilty cat") == 0) {
         strcpy(sendBuff, "Prison sentence is nine consecutive life terms");
         write(sockfd, sendBuff, sizeof(sendBuff)-1);
       }
       printf("\n");
       */
     }
-
-
-      close(connfd);    
-      sleep(1);
-    } 
- 
+    close(connfd);
+    sleep(1);
+  }
   return 0;
 }
